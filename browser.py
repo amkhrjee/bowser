b8d57e43409c93a29b332b54d0e434eb0b762c38
amkhrjee <aniruddha.mukherji@outlook.com>
2024-04-19 19:36:33 +0530

    add: support for file

--- browser.py
+++ browser.py
@@ -5,14 +5,16 @@ import ssl
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["https", "http"]
+        assert self.scheme in ["https", "http", "file"]
 
         if "/" not in url:
             url = url + "/"
         self.host, url = url.split("/", 1)
+
         if ":" in self.host:
             self.host, port = self.host.split(":", 1)
             self.port = int(port)
+
         self.path = "/" + url
 
         if self.scheme == "https":
@@ -21,34 +23,39 @@ class URL:
             self.port = 80
 
     def request(self):
-        s = socket.socket(
-            family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
-        )
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+        if self.scheme in ["https", "http"]:
+            s = socket.socket(
+                family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+            )
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        s.connect((self.host, self.port))
-        request = f"GET {self.path} HTTP/1.0\r\n"
-        request += f"Host: {self.host}\r\n"
-        request += "\r\n"
-        s.send(request.encode("utf8"))
+            s.connect((self.host, self.port))
+            request = f"GET {self.path} HTTP/1.0\r\n"
+            request += f"Host: {self.host}\r\n"
+            request += "Connection: close\r\n"
+            request += "\r\n"
+            s.send(request.encode("utf8"))
 
-        # Receiving response
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-        statuslne = response.readline()
-        version, status, explanation = statuslne.split(" ", 2)
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n":
-                break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
-            assert "transfer-encoding" not in response_headers
-            assert "content-encoding" not in response_headers
-        content = response.read()
-        s.close()
+            # Receiving response
+            response = s.makefile("r", encoding="utf8", newline="\r\n")
+            statuslne = response.readline()
+            version, status, explanation = statuslne.split(" ", 2)
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n":
+                    break
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
+                assert "transfer-encoding" not in response_headers
+                assert "content-encoding" not in response_headers
+            content = response.read()
+            s.close()
+        elif self.scheme == "file":
+            with open(self.path) as f:
+                content = f.read()
         return content
 
 
@@ -71,4 +78,7 @@ def load(url):
 if __name__ == "__main__":
     import sys
 
-    load(URL(sys.argv[1]))
+    if len(sys.argv) == 1:
+        load(URL("file:///home/amkhrjee/Code/bowser/test.txt"))
+    else:
+        load(URL(sys.argv[1]))
